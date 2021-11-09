158d5f54e20eed6e609b1d818f374ee3597943ba
Vitalii <84464769+Pokhyl@users.noreply.github.com>
2021-11-09 18:46:53 +0100

    HomeWork2

--- app/src/main/java/com/example/homework/BlankFragment1.kt
+++ app/src/main/java/com/example/homework/BlankFragment1.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
@@ -19,21 +21,25 @@ class BlankFragment1 : Fragment() {
     @Inject
     lateinit var cat: Cat
     @Inject
-   lateinit var viewModelFactory: ViewModelFactory
-lateinit var viewModel: HomeWorkViewModel
+   lateinit var viewModelFactory: ViewModelProvider.Factory
+    private val viewModel: HomeWorkViewModel by viewModels {
+        viewModelFactory
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProviders.of(this, viewModelFactory)
-            .get(HomeWorkViewModel::class.java)
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank1, container, false)
         (requireActivity() as MainActivity).homeWorkComponent.inject(this)
+//        viewModel = ViewModelProviders.of(this, viewModelFactory)
+//            .get(HomeWorkViewModel::class.java)
+println("$viewModelFactory !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         println(dog)
         println(parrot.name)
         println(cat.name+" "+cat.age)
-        println(viewModel)
+        viewModel.liveData.observe(viewLifecycleOwner,{ println(it+"????????????????????????????????????????????????????????")} )
+        return inflater.inflate(R.layout.fragment_blank1, container, false)
+
+
     }
 }
